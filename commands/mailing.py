7d1ce29a6970febf4ceb58c64e9b58ec74d847cc
Batya02 <magomedov.abdul20012@gmail.com>
2021-06-28 07:32:44 +0300

    adds blocked users stat

--- commands/mailing.py
+++ commands/mailing.py
@@ -2,6 +2,7 @@ from objects.globals import dp, bot
 
 from aiogram.types import Message
 from aiogram.dispatcher.storage import FSMContext
+from aiogram.utils.exceptions import BotBlocked, UserDeactivated, ChatNotFound
 
 from states.states import Mailing
 
@@ -23,15 +24,23 @@ async def get_mailing_text(message: Message, state:FSMContext):
     
     start_time = dt.now()
 
+    blocked:int = 0
+
     for i, user in enumerate(users):
         if i % 5 == 0:
             await sleep(1)
-
-        await bot.send_message(
-            user.user_id, 
-            text=message.text
-        )
+        try:
+            await bot.send_message(
+                user.user_id, 
+                text=message.text
+            )
+        except (BotBlocked, UserDeactivated, ChatNotFound):
+            blocked += 1
     
+    with open(r"temp/blocked_users.txt", "w") as add_blocked_count_users:
+        add_blocked_count_users.write(str(blocked))
+        add_blocked_count_users.close()
+
     end_time = int((dt.now() - start_time).total_seconds())
     
     await message.answer(
