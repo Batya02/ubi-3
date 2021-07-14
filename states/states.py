7b8e883971ef320cf849f2516e9584f769666787
Batya02 <magomedov.abdul20012@gmail.com>
2021-07-14 18:26:52 +0300

    add state SendMessage

--- states/states.py
+++ states/states.py
@@ -2,3 +2,6 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 class Mailing(StatesGroup):
     mailing_text_targ = State()
+
+class SendMessage(StatesGroup):
+    send_message_targ = State()
