f8b32cd1df4ea80718ef5d194c099bf43677edda
Batya02 <magomedov.abdul20012@gmail.com>
2021-07-15 18:05:58 +0300

    fixed handler

--- commands/send_message.py
+++ commands/send_message.py
@@ -5,13 +5,21 @@ from aiogram.dispatcher.storage import FSMContext
 
 from states.states import SendMessage
 
+from temp.lang_keyboards import lang_keyboard
+
 @dp.message_handler(lambda message: message.text == "✉️Отправить сообщение")
 async def send_message(message: Message):
-    await message.answer("Напишите сообщение администратору.\nОтвет поступит через некоторое время:")
+    await message.answer("\nОтменить отправку сообщения -> /start\nНапишите сообщение администратору.\nОтвет поступит через некоторое время:")
     await SendMessage.send_message_targ.set()
 
-@dp.message_handler(state=SendMessage.send_message_targ)
+@dp.message_handler(
+        lambda message: message.text not in [k[0] for k in lang_keyboard["RU"]], 
+        state=SendMessage.send_message_targ)
 async def send_message_process(message: Message, state:FSMContext):
+    if message.text == "/start":
+        await state.finish()
+        return await message.answer("Отмена")
+    
     await state.finish()
     username = f"@{message.from_user.username}" if not None else "Unknow"
     await bot.send_message(
