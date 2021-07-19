1854c72e88d1690e602ee78e6c1a69ba8362f131
Batya02 <magomedov.abdul20012@gmail.com>
2021-07-19 11:09:49 +0300

    add commentaries

--- commands/get_numbers.py
+++ commands/get_numbers.py
@@ -11,15 +11,19 @@ from json import loads
 
 @dp.message_handler(lambda message: message.text == "📲Купить виртуальный номер")
 async def get_numbers(message:Message):
-    host_site_main = config["host_site_main"]
+
+    host_site_main = config["host_site_main"] # Host main
+
+    # Get all data from services
     async with ClientSession() as client_session:
         async with client_session.get(f"https://{host_site_main}/v1/guest/products/russia/any") as all_services:
             data_services = loads(await all_services.text())
             await client_session.close()
         
-        tg_price = int(data_services["telegram"]["Price"]) + 1
-        vk_price = int(data_services["vkontakte"]["Price"]) + 1
-        wa_price = int(data_services["whatsapp"]["Price"]) + 1
+        # Add percent
+        tg_price = int(data_services["telegram"]["Price"])  + 1 # Telegram price
+        vk_price = int(data_services["vkontakte"]["Price"]) + 1 # Vkontakte price
+        wa_price = int(data_services["whatsapp"]["Price"])  + 1 # Whats app price
         
         prices_markup = InlineKeyboardMarkup(
             inline_keyboard=[
